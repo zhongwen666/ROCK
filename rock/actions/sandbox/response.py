c9e1ea96c8043fa54fa03b7bb79d93674eb5a391
jiaoliao <lizhongwen.lzw@alibaba-inc.com>
2026-07-06 01:59:24 +0000

    opt get sttus

--- rock/actions/sandbox/response.py
+++ rock/actions/sandbox/response.py
@@ -43,8 +43,8 @@ class IsAliveResponse(BaseModel):
 
 class SandboxStatusResponse(BaseModel):
     sandbox_id: str = None
-    status: dict = None
-    port_mapping: dict = None
+    status: dict | None = None
+    port_mapping: dict | None = None
     host_name: str | None = None
     host_ip: str | None = None
     is_alive: bool = True
